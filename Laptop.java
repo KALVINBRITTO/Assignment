80235222465f3aaaeeb6820f39d3d5c88b44e942
KALVINBRITTO <153616005+KALVINBRITTO@users.noreply.github.com>
2023-12-13 13:29:42 +0530

    Add files via upload

---
+++ Laptop.java
@@ -0,0 +1,32 @@
+package week1.day2;
+
+public class Laptop {
+	
+	public static void main(String[] args) {
+		
+		String laptop= "DELL";
+		System.out.println("laptop =" + laptop );
+		int laptopPrice=45000;
+		System.out.println("laptopPrice =" + laptopPrice);
+		float dimension= 123.456f;
+		System.out.println("dimension = " + dimension);
+		long serialNumber=73584210l;
+		System.out.println("serialNumber =" + serialNumber);
+		double length=999999.99999;
+		System.out.println("lenght ="+ length );
+		short quantity=4;
+		System.out.println("quantity ="+ quantity);
+		byte pack=2;
+		System.out.println("pack ="+ pack);
+		boolean productDamaged= false;
+		System.out.println("productDamaged =" + productDamaged);
+		char series='Z';
+		System.out.println("series ="+ series);
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Student.java
@@ -0,0 +1,29 @@
+package week1.day2;
+
+public class Student {
+	
+	public static void main(String[] args) {
+		
+	  String student ="studentDetails,name,mobileNumber,address,rollNo,bloodGroup";
+	  System.out.println("student =" + student);
+	  int studentList=35;
+	  System.out.println("studentList ="+ studentList);
+	  float passPercentage=90.99f;
+	  System.out.println("passPercentage ="+ passPercentage);
+	  long mobileNumber=735842104l;
+	  System.out.println("mobilenumber ="+ mobileNumber);
+	  double attendencePercent=75.55;
+	  System.out.println("attendencePercent ="+ attendencePercent);
+	  short rollNo=17;
+	  System.out.println("rollNo ="+ rollNo);
+	  byte studentHeight=125;
+	  System.out.println("studenHeight ="+ studentHeight);
+	  boolean studentsAvailable= false;
+	  System.out.println("studentsAvailable ="+ studentsAvailable);
+	  char bloodGroup='O';
+	  System.out.println("bloodGroup ="+bloodGroup);
+	  
+		
+	}
+
+}
